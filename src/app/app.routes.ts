87a84949d426064d7c207f5f8a57c8d7f4fd0306
Sufyan Ali <sufyanali3157426@gmail.com>
2026-07-17 22:06:00 +0200

    basic layout created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ReviewPage } from '../page/review-page/review-page';
+import { MainLayout } from '../layout/main-layout/main-layout';
+import { Header } from '../layout/header/header';
+export const routes: Routes = [
+    {
+        path: '',
+        component:MainLayout,
+        children: [
+            {path:'',component:ReviewPage},
+        ]
+    }
+];

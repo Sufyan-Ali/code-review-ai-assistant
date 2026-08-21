1ecdf475d4add28af7d91cd4ef104cff947684f3
Sufyan Ali <sufyanali3157426@gmail.com>
2026-08-21 15:47:25 +0200

    reactive forms implemented.

--- src/features/review/components/code-input-component/code-input-component.ts
+++ src/features/review/components/code-input-component/code-input-component.ts
@@ -1,30 +1,37 @@
-import { Component,output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, output } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReviewRequest } from '../../models/review-request.model';
 
 @Component({
   selector: 'app-code-input-component',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule,FormsModule],
   templateUrl: './code-input-component.html',
   styleUrl: './code-input-component.css',
 })
 export class CodeInputComponent {
-  selectedLanguage: string = ""
-  selectedReviewFocus: string = ""
-  enteredCode: string = ""
-  submittedData :ReviewRequest  = {
-      "language" : "",
-      "reviewFocus" : "",
-      "code" : ""
-  }
+  private fb = inject(FormBuilder)
+  // selectedLanguage: string = ""
+  // selectedReviewFocus: string = ""
+  // enteredCode: string = ""
+  data = this.fb.group({
+    "language": [""],
+    "reviewFocus": [""],
+    "code": [""]
+  })
+  submittedData: ReviewRequest = {
+    "language": "",
+    "reviewFocus": "",
+    "code": ""
+  }  
   reviewSubmitted = output<ReviewRequest>()
 
   submitReview() {
     this.submittedData = {
-      "language" : this.selectedLanguage,
-      "reviewFocus" : this.selectedReviewFocus,
-      "code" : this.enteredCode
+      "language": this.data.value.language ?? "",
+      "reviewFocus": this.data.value.reviewFocus ?? "",
+      "code": this.data.value.code ?? ""
     }
-    this.reviewSubmitted.emit(this.submittedData)
+    console.log(this.data.value);
+    this.reviewSubmitted.emit(this.submittedData) 
   }
 }
